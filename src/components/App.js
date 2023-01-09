f6de18e1a5d187de76105a0ad31d180675e56ac1
kevinkkimutai <kiruikevinkim@gmail.com>
2023-01-09 18:36:04 +0300

    Assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
     </nav>
-  );
+  )
 }
 
 function Home() {
@@ -13,20 +13,29 @@ function Home() {
     <div id="home">
       <h1>Home</h1>
     </div>
-  );
+  )
 }
 
-{/* write an <About> component here */}
+//  write an <About> component here 
 
+const About = () => {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
